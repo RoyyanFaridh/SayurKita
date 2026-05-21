3486ea51d31184e22a1c63e969a26b5ad4c2faba
Marici Sianturi <maricisianturi1@gmail.com>
2026-05-21 21:41:49 +0700

    update

--- ai/utils/fraud.py
+++ ai/utils/fraud.py
@@ -3,6 +3,12 @@ from datetime import datetime, date
 
 aksi_user = {}  
 
+HARGA_POIN = {
+    "input_bahan": 10,
+    "bagi_surplus": 50,
+    "klaim_surplus": 20
+}
+
 def cek_aksi(user_id, aksi):
     now = datetime.now()
     hari_ini = date.today()
@@ -17,15 +23,17 @@ def cek_aksi(user_id, aksi):
         data["hari"] = hari_ini
     
     if data["count"] >= 30:
-        return False, "Batas aksi harian 30, coba lagi besok."
+        return False, "Batas aksi harian 30, coba lagi besok.", 0
     
     if data["count"] > 0:
         selisih = (now - data["last"]).total_seconds()
         if selisih < 60:
             tunggu = round(60 - selisih)
-            return False, f"Tunggu {tunggu} detik lagi."
+            return False, f"Tunggu {tunggu} detik lagi.", 0
     
 
     data["count"] += 1
     data["last"] = now
-    return True, "Aksi diperbolehkan"
+    
+    poin_didapat = HARGA_POIN.get(aksi, 0)
+    return True, "Aksi diperbolehkan", poin_didapat
